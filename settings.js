6c6272d249e2bc90db82e7418f9839c512ec7bd6
Pakaya480 <Sachipky61@gmail.com>
2025-03-10 08:31:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = { 
+module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=u7JBELxY#Kd984XkjpYSFNOwrRzfUiJTpbUojkkNFjhnnOawMpF4' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
